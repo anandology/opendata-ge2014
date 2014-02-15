0acc8702f2189a0c01339e342d0f4ccfb1fba7e0
Srinivas Adivi <ssrinivas@zeomega.com>
2014-02-15 21:00:07 +0530

    Fixed: issue #2

--- scripts/parse_wikipedia_listing.py
+++ scripts/parse_wikipedia_listing.py
@@ -6,7 +6,8 @@ d = [line.strip().split("\t") for line in open(sys.argv[1])]
 
 if "--ac" in sys.argv:
     re_x = re.compile(",| and ")
-    re_num = re.compile(" *(\d+)(.*)")
+    re_num1 = re.compile(" *(\d+)(.*)")
+    re_num2 = re.compile(r" *([0-9]+[\D]+)")
 
     for row in d:
         i, pcname, category, ac = row
@@ -14,10 +15,12 @@ if "--ac" in sys.argv:
         #print i, pcname, names
         for name in names:
             #print name
-            n, name = re_num.match(name).groups()
-            name = name.strip(". ")
-            #print i, pcname, n, name
-            print "PC%02d\tAC%03d\t%s" % (int(i), int(n), name)
+            for name in re_num2.findall(name):
+                #print name
+            	n, name = re_num1.match(name).groups()
+            	name = name.strip(". ")
+                #print i, pcname, n, name
+                print "PC%02d\tAC%03d\t%s" % (int(i), int(n), name)
 else:
     for row in d:
         print "PC%02d\t%s" % (int(row[0]), row[1])
