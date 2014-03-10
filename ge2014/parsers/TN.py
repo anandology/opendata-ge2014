79d664f0ac0aa319fc93ac306a3b542be4db054a
Anand Chitipothu <anandology@gmail.com>
2014-03-10 21:57:14 +0530

    Generated PC and AC listing for TN.

---
+++ ge2014/parsers/TN.py
@@ -0,0 +1,14 @@
+import re
+import urllib2
+
+from . import cache
+from . import wikipedia
+from bs4 import BeautifulSoup
+
+@cache.disk_memoize("data/TN/ac.tsv")
+def get_acs():
+    return wikipedia.get_ac_list("Tamil Nadu")
+
+@cache.disk_memoize("data/TN/pc.tsv")
+def get_pcs():
+    return wikipedia.get_ac_list("Tamil Nadu")
